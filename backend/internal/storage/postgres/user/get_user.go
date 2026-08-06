abdb746c789ddb86aa4deb79ba3fad9c3785fdae
Вячеслав Смирнов <smslava7@gmail.com>
2026-08-06 13:06:04 +0300

    feat: get user by email repository method

--- backend/internal/storage/postgres/user/get_user.go
+++ backend/internal/storage/postgres/user/get_user.go
@@ -49,3 +49,42 @@ func (r *UsersRepository) GetUser(
 
 	return userDomain, nil
 }
+
+func (r *UsersRepository) GetUserByEmail(
+	ctx context.Context,
+	email string,
+) (domain.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+	SELECT id, nickname, email, password_hash, created_at 
+	FROM users 
+	WHERE email=$1
+	`
+
+	row := r.pool.QueryRow(ctx, query, email)
+
+	var userModel UserModel
+	err := row.Scan(
+		&userModel.ID,
+		&userModel.Nickname,
+		&userModel.Email,
+		&userModel.PasswordHash,
+		&userModel.CreatedAt,
+	)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return domain.User{}, fmt.Errorf(
+				"user with email %s not found: %w",
+				email, core_errors.ErrNotFound,
+			)
+		}
+
+		return domain.User{}, fmt.Errorf("scan user: %w", err)
+	}
+
+	userDomain := userDomainFromModel(userModel)
+
+	return userDomain, nil
+}
